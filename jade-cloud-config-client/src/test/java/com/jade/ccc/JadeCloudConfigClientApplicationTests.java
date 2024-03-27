67e5e5e74b2f3471f65bad0cc7ac133ca03a1c7e
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2024-03-27 19:19:34 +0530

    Cloud config demo

---
+++ jade-cloud-config-client/src/test/java/com/jade/ccc/JadeCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jade.ccc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadeCloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ jade-cloud-config-demo/src/test/java/com/jade/cc/JadeCloudConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jade.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadeCloudConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
